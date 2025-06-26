b4ab1c50bd82283687e351f559faf1c8477b79cb
Egurchik <zhora-sokolovskiy@mail.ru>
2025-06-26 19:00:01 +0300

    Инициализация проекта

---
+++ src/main/java/com/mycompany/couple/Couple.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.couple;
+
+/**
+ *
+ * @author zhora
+ */
+public class Couple {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
